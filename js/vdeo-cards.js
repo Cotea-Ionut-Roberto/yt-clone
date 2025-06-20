73ec9ff95826b84a3b8c3327340199759076b646
Cotea Ionut <coteaionutroberto@gmail.com>
2025-06-20 20:05:29 +0300

    Create array of objects for video cards

---
+++ js/vdeo-cards.js
@@ -0,0 +1,105 @@
+const videos = [
+    {
+        thumbnail: 'assets/images/thumbnails/thumbnail1.avif',
+        title: 'Lofi Hip Hop Beats 📮 1980s & 90s Retro Vibes & Nostalgic Japanese Town Ambience 🌆 Lofi Rain Playlist',
+        duration: '11:13:25',
+        views: 47161,
+        publishedDate: '2025-05-29',
+        author: {
+            name: 'The Japanese Town',
+            profilePic: 'assets/images/channel-profile-pictures/channel1.jpg'
+        }
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail2.jpg",
+        title: "2024 Lamborghini Revuelto Review - The Fastest Lamborghini Ever Made",
+        duration: "21:42",
+        views: 182000,
+        publishedDate: "2025-01-20",
+        author: {
+            name: "Vehicle Virgins",
+            profilePic: "assets/images/channel-profile-pictures/channel2.jpg"
+        }
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail3.avif",
+        title: "Healing Place - Spiritual Tibetan Ambient Music - Meditative Relaxing Ambience",
+        duration: "1:00:01",
+        views: 7700,
+        publishedDate: "2025-06-18",
+        author: {
+            name: "Eternal Depth",
+            profilePic: "assets/images/channel-profile-pictures/channel3.jpg"
+        }
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail4.jpg",
+        title: "quitting your youtube addiction is easy, actually",
+        duration: "5:10",
+        views: 1200000,
+        publishedDate: "2024-12-20",
+        author: {
+            name: "easy, actually",
+            profilePic: "assets/images/channel-profile-pictures/channel4.jpg"
+        }
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail5.avif",
+        title: "Maximize Productivity, Physical & Mental Health With Daily Tools | Huberman Lab Essentials",
+        duration: "31:53",
+        views: 221000,
+        publishedDate: "2025-06-06",
+        author: {
+            name: "Andrew Huberman",
+            profilePic: "assets/images/channel-profile-pictures/channel5.jpg"
+        }
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail6.avif",
+        title: "Lexus LFA Review // A Living Legend",
+        duration: "20:18",
+        views: 3400000,
+        publishedDate: "2022-06-20",
+        author: {
+            name: "Throttle House",
+            profilePic: "assets/images/channel-profile-pictures/channel6.jpg"
+        }
+
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail7.avif",
+        title: "FULL CHEST WORKOUT W. COACH HANY RAMBOD",
+        duration: "26:43",
+        views: 1300000,
+        publishedDate: "2024-06-20",
+        author: {
+            name: "Chris Bumstead",
+            profilePic: "assets/images/channel-profile-pictures/channel7.jpg"
+        }
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail8.avif",
+        title: "RTX 5090 in GTA 5 Enhanced Edition LOOKS INCREDIBLE - INSANE Ray Tracing Realistic Graphics 4K120FPS",
+        duration: "3:23:54",
+        views: 195000,
+        publishedDate: "2025-04-20",
+        author: {
+            name: "RTXGameplays",
+            profilePic: "assets/images/channel-profile-pictures/channel8.jpg"
+        }
+    },
+    {
+        thumbnail: "assets/images/thumbnails/thumbnail9.avif",
+        title: "Inside a Single-Engine Aircraft | How a Cessna 172 Works",
+        duration: "23:28",
+        views: 1200000,
+        publishedDate: "2024-06-20",
+        author: {
+            name: "Joyplanes",
+            profilePic: "assets/images/channel-profile-pictures/channel9.jpg"
+        }
+    }
+
+
+
+];
